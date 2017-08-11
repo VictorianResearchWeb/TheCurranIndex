324968d8b9be1fbf6e795e8c7b4d1a4b98827461
Trisha <trishablewis@gmail.com>
2017-08-11 14:44:01 -0400

    updated article display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,15 @@ class ArticlesController < ApplicationController
     @periodicals = Periodical.order(:name)
 
     @periodical = Periodical.find_by(id: 1)
-    @list = Article.includes(:periodical).where(periodical_id: @periodical.id).group_by(&:volume)
     
+    @list = @periodical.articles.includes(:periodical, :month).group_by(&:volume)
+
     @articles = Article.order(:title).paginate(:page => params[:page], :per_page => 30)
   end
 
   def contributor
     @contributor = Contributor.find_by(id: 1)
-    @contributor_articles = @contributor.articles.includes(:periodical)
+    @contributor_articles = @contributor.articles.includes(:periodical, :month)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ActiveRecord::Base
+#  has_many :article_contributor_links
+#  has_many :contributors, through: :article_contributor_links
   has_and_belongs_to_many :contributors
   belongs_to :periodical
   belongs_to :month
+
+  scope :order_by_volume, -> { order(volume: :asc)}
 
   #concatenate date information for display
   def date
@@ -10,23 +14,6 @@ class Article < ActiveRecord::Base
 
   def pages
     "#{self.page_start} - #{self.page_end}"
-  end
-
-  #create string of article information for periodical citation
-  def periodical_citation
-    
-    all_contributors = []
-    self.contributors.each do |c|
-      all_contributors << c.first_last_name
-    end
-    
-    array = [self.periodical.abbreviation, self.code, self.title, self.pages, all_contributors, self.attribution_confidence, self.attribution]
-    citation = array.reject(&:blank?).join(", ").to_s
-
-    unless self.article_type == "prose"
-      citation = citation + " [#{self.article_type}]"
-    end
-    return citation
   end
 
   def author_citation
@@ -38,5 +25,19 @@ class Article < ActiveRecord::Base
     end
     return citation
   end
+
+#create string of article information for periodical citation
+  def periodical_citation
+    all_contributors = self.contributors.map{|contributor| contributor.first_last_name}
+
+    array = [self.periodical.abbreviation, self.code, self.title, self.pages, all_contributors, self.attribution_confidence, self.attribution]
+    citation = array.reject(&:blank?).join(", ").to_s
+
+    unless self.article_type == "prose"
+      citation = citation + " [#{self.article_type}]"
+    end
+    return citation
+  end
+
 
 end
