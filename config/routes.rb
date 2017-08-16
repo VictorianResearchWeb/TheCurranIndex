207284d8d5dc8dd2c92de1e8b9fcaf46db4cc26b
Trisha <trishablewis@gmail.com>
2017-08-16 11:00:47 -0400

    removed test view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'articles#index'
   get '/contributor' => 'articles#contributor', as: :contributor
   get '/periodical' => 'articles#periodical', as: :periodical
-  get '/test' => 'articles#test', as: :test
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
