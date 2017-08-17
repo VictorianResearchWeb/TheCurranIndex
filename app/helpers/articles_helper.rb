2f259cb48157ec7e771ac8ecebb2bd9741fedd7a
Trisha <trishablewis@gmail.com>
2017-08-17 11:17:46 -0400

    filter links

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,28 @@
 module ArticlesHelper
+
+  def selected(filter_name)
+    filter = @search.filter(filter_name)
+    if filter_name == :contributors
+      @link = {}
+      filter.selected.each do |entity|
+        @entity_name = entity.first_last_name
+        @link.store(@entity_name, entity)
+      end
+    elsif filter_name == :periodical
+      @link = {}
+      filter.selected.each do |entity|
+        @entity_name = entity.title
+        @link.store(@entity_name, entity)
+      end
+
+    elsif filter_name == :article_type
+      @link = {}
+      filter.selected.each do |entity|
+        @link.store(entity, entity)
+      end
+      
+    end
+    return @link
+  end
+
 end
