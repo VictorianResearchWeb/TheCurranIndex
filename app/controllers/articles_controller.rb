0c6731abae0ed95486596d51dbfac4400270528e
Trisha <trishablewis@gmail.com>
2017-08-23 14:14:36 -0400

    added contributor identifier

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   def index
-    @periodicals = Periodical.order(:title)
     @search = ArticleSearch.new(params)
     @list = @search.result.includes(:contributors, :periodical, :month).periodical_order.contents_order.paginate(page: params[:page], :per_page => 20)
   end
