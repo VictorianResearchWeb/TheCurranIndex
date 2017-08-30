6cade1b0a7c7024aa9dfb8bc6788841c80e5e29a
Trisha <trishablewis@gmail.com>
2017-08-30 13:34:13 -0400

    release notes styling

--- app/models/essay.rb
+++ app/models/essay.rb
@@ -1,5 +1,10 @@
 class Essay < ActiveRecord::Base
-  belongs_to :month
+  belongs_to :month, foreign_key: "pub_month"
 
   scope :date_order, -> { order(pub_year: :desc).order(pub_month: :desc)}
+
+  #used to display release note essays
+  def pub_date
+    "#{self.month.publication_month} #{self.pub_year}"
+  end
 end
